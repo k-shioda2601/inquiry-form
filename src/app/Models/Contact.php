e4c6500798463ea8e80407b191caa65418e36089
k-shioda40 <Kento.shioda@gmail.com>
2026-04-13 08:11:44 +0900

    add models

--- src/app/Models/Contact.php
+++ src/app/Models/Contact.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Category;
 
 class Contact extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'first_name',
+        'last_name',
+        'gender',
+        'email',
+        'tel',
+        'address',
+        'building',
+        'detail',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
